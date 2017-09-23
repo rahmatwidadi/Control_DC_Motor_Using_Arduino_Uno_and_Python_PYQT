a4db525526f8d0541005066922893b63548130d1
rahmatwidadi <rahmatwidadi@users.noreply.github.com>
2017-09-23 09:21:11 +0700

    Add files via upload

---
+++ firmware_tugas_pyqt_arduino.ino
@@ -0,0 +1,122 @@
+//data serial
+int inbytes = 1;
+byte RXBuf [64];
+//int jalan = 0;
+
+// motor satu
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+
+// motor dua
+int enB = 5;
+int in3 = 7;
+int in4 = 6;
+
+void setup()
+{
+// set all the motor control pins to outputs
+Serial.begin(9600);
+pinMode(enA, OUTPUT);
+pinMode(enB, OUTPUT);
+pinMode(in1, OUTPUT);
+pinMode(in2, OUTPUT);
+pinMode(in3, OUTPUT);
+pinMode(in4, OUTPUT);
+}
+
+void stay()
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite (enA, 0);
+  analogWrite (enB, 0);
+}
+
+void forward()
+{
+  //motor 1
+  digitalWrite (in1, HIGH);
+  digitalWrite (in2,  LOW);
+  analogWrite (enA, 100);
+  //motor 2
+  digitalWrite (in3, HIGH);
+  digitalWrite (in4, LOW);
+  analogWrite (enB, 100);
+}
+
+void reverse()
+{
+  //motor 1
+  digitalWrite (in1,  LOW);
+  digitalWrite (in2,  HIGH);
+  analogWrite (enA, 100);
+  //motor 2
+  digitalWrite (in3, LOW);
+  digitalWrite (in4, HIGH);
+  analogWrite (enB, 100);
+}
+
+void turn_left()
+{
+  //motor 1
+  digitalWrite (in1, HIGH);
+  digitalWrite (in2,  LOW);
+  analogWrite (enA, 80);
+  //motor 2
+  digitalWrite (in3, HIGH);
+  digitalWrite (in4, LOW);
+  analogWrite (enB, 40);
+}
+
+void turn_right()
+{
+  //motor 1
+  digitalWrite (in1, HIGH);
+  digitalWrite (in2,  LOW);
+  analogWrite (enA, 40);
+  //motor 2
+  digitalWrite (in3, HIGH);
+  digitalWrite (in4, LOW);
+  analogWrite (enB, 80);
+}
+
+
+void loop()
+{
+  if (Serial.available()) 
+  {
+    int inbytes = Serial.readBytes(RXBuf,sizeof(RXBuf));
+    //int inbytes = Serial.readBytes(RXbuf,sizeof(RXbuf));
+    switch (inbytes) {
+      case 1:
+        stay();
+        Serial.print("Stop");
+        break;
+      case 2:
+        forward();
+        Serial.print("Constan Forward");
+        //delay(3000);
+        break;
+      case 3:
+        turn_right();
+        Serial.print("Constan Turn Rigt");
+        break;
+      case 4:
+        reverse();
+        Serial.print("Constan Backward");
+        break;
+      case 5:
+        turn_left();
+        Serial.print("constan Turn Left");
+        break;
+      default:
+        stay();
+        Serial.print("Stop");
+    }
+  }
+   
+}
+
